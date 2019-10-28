4b505178ca6ad9dbeafd6317dea6766d20b29238
Nina Zhang <ninazhang@exosite.com>
2019-10-28 10:32:03 +0800

    Fix python3 issue on iot communication (Issue: MUR-10451)

--- endpoints/websocket/websocket.lua
+++ endpoints/websocket/websocket.lua
@@ -1,7 +1,7 @@
 --#ENDPOINT WEBSOCKET /ws
 -- Doc: http://docs.exosite.com/reference/services/websocket/#websocket_info
 if (websocketInfo.type == "open") then
-  websocketInfo.send("Welcome")
+  websocketInfo.send("Welcomerr")
 elseif (websocketInfo.type == "data") then
   -- Print the incoming message and close the connection
   print(websocketInfo.message)
